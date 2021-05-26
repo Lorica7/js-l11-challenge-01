2306c527067300287d7bfbfbcafd5c771735c238
Lorica7 <38511443+Lorica7@users.noreply.github.com>
2021-05-26 12:26:59 -0400

    fetching data

--- js/script.js
+++ js/script.js
@@ -2,3 +2,13 @@ const button = document.querySelector("button");
 const authorSpan = document.querySelector(".author");
 const imgDiv = document.querySelector(".image-container");
 const img = document.querySelector(".img");
+
+
+const getImage = async function () {
+    const url = "https://picsum.photos/v2/list?limit=10";
+    const res = await fetch(url)
+    const images = await res.json();
+    console.log(images);
+}
+
+getImage();
